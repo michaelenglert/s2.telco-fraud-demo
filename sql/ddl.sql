662408d939d6dff7c4157c0af4f566f28fe03b85
Michael Englert <m.e@tuta.com>
2024-06-29 00:04:09 +0100

    added new FTS Table

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,4 +1,9 @@
-CREATE TABLE `usage` (
+-- Create and use the main Database
+CREATE DATABASE IF NOT EXISTS telco;
+USE telco;
+
+-- Create usage Table
+CREATE TABLE IF NOT EXISTS `usage` (
     UsageID VARCHAR(50) NOT NULL,
     SIMID VARCHAR(50),
     Location VARCHAR(100),
@@ -10,10 +15,17 @@ CREATE TABLE `usage` (
     SHARD KEY (UsageID)
 );
 
-CREATE TABLE `potential_fraud` (
+-- Create potential_fraud Table
+CREATE TABLE IF NOT EXISTS `potential_fraud` (
     FraudID BIGINT NOT NULL AUTO_INCREMENT,
     UsageID1 VARCHAR(50),
     UsageID2 VARCHAR(50),
     Timestamp TIMESTAMP(6),
     SHARD KEY (FraudID)
 );
+
+-- Create Table with new Full-Text Index over JSON
+CREATE TABLE `telco-users-fts` (
+    id BSON,
+    records JSON,
+    FULLTEXT USING VERSION 2 rec_ft_index (records));
