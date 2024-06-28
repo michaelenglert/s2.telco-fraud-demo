7bc11e59269d8743c32afc76a7eb8d055f7a16c1
Michael Englert <m.e@tuta.com>
2024-06-28 10:16:34 +0100

    added DDL visual

---
+++ ddl.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `usage` (
+    UsageID BIGINT NOT NULL AUTO_INCREMENT,
+    IMEI VARCHAR(50),
+    SIMID VARCHAR(50),
+    Location VARCHAR(100),
+    UsageType VARCHAR(255),
+    DataTransferAmount NUMERIC(18,2),
+    CallDuration INT,
+    Timestamp TIMESTAMP(6),
+    Geo GEOGRAPHYPOINT,
+    SHARD KEY (UsageID)
+);
+
+CREATE TABLE `potential_fraud` (
+    FraudID BIGINT NOT NULL AUTO_INCREMENT,
+    UsageID1 BIGINT,
+    UsageID2 BIGINT,
+    Timestamp TIMESTAMP(6),
+    SHARD KEY (FraudID)
+);
