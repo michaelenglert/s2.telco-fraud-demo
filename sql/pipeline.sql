fdad06cc05df361ac670fa8e02b9069802a9d8b0
Michael Englert <m.e@tuta.com>
2024-06-28 20:41:19 +0100

    added pipelines

--- sql/pipeline.sql
+++ sql/pipeline.sql
@@ -1,12 +1,13 @@
--- BASIC
-CREATE OR REPLACE AGGREGATOR PIPELINE loadFrauds AS
-LOAD DATA S3 's3://s2db-demos-pub-bucket/telco-fraud/'
+-- Get CSV Data
+CREATE OR REPLACE PIPELINE loadFrauds AS
+LOAD DATA S3 's3://menglert-us/telco-fraud/'
 CONFIG '{"region":"us-east-1"}'
-INTO TABLE `usage`
+INTO PROCEDURE fraud
 IGNORE 1 LINES
 FORMAT CSV
 FIELDS TERMINATED BY ','
 (
+    UsageID,
     SIMID,
     Location,
     UsageType,
@@ -17,7 +18,17 @@ FIELDS TERMINATED BY ','
 );
 
 TEST PIPELINE loadFrauds LIMIT 2;
-START PIPELINE loadFrauds;
 
-STOP PIPELINE loadFrauds;
-DROP PIPELINE loadFrauds;
+-- Get Mongo Data
+CREATE LINK mongoTelcoUsers AS MONGODB
+CONFIG '{"mongodb.hosts":"<redacted>:27017",
+"collection.include.list": "telco.telco-users",
+"mongodb.ssl.enabled":"true",
+"mongodb.authsource":"admin"}'
+CREDENTIALS '{"mongodb.user":"me",
+"mongodb.password":"<redacted>"}';
+
+CREATE TABLES AS INFER PIPELINE AS LOAD DATA LINK mongoTelcoUsers '*' FORMAT AVRO;
+
+-- Run Pipelines
+START ALL PIPELINES;
